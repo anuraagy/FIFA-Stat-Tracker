600c53addde3f01b06c046cd544a3e282f898716
Anuraag Yachamaneni <ayachama@purdue.edu>
2017-01-04 12:28:43 -0500

    Finish adding validations and presorting of table

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -42,32 +42,19 @@ class GamesController < ApplicationController
 
     if current_user.name == @game.home_user
       @game.reviewer = @game.away_user
-    else
+    elsif current_user.name == @game.away_user
       @game.reviewer = @game.home_user
     end
 
-    if @game.save
-      @home_user = User.find_by(:name => @game.home_user)
-      @home_user.goals_for += @game.home_score
-      @home_user.goals_against += @game.away_score
-      @away_user = User.find_by(:name => @game.away_user)
-      @away_user.goals_for += @game.away_score
-      @away_user.goals_against += @game.home_score
-
-      if @game.home_score > @game.away_score || (@game.home_penalty_score? && @game.home_penalty_score > @game.away_penalty_score)
-        @home_user.points += 3
-        @home_user.win_count += 1
-        @away_user.loss_count += 1
-      else
-        @away_user.points += 3
-        @away_user.win_count += 1
-        @home_user.loss_count += 1
-      end
-      @home_user.save
-      @away_user.save
-      redirect_to @game
-    else
+    if current_user.name != @game.home_user && current_user.name != @game.away_user
+      @game.errors.add(:base, "You can't submit games for others! Please make sure that one of the players is you.")
       render :new
+    else
+      if @game.save
+        redirect_to @game
+      else
+        render :new
+      end
     end
   end
 
