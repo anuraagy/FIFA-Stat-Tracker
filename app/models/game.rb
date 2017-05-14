03ba0cb6f0e9164ab73ecb0d0d794b292de9e5cc
Anuraag Yachamaneni <ayachama@purdue.edu>
2017-05-13 23:51:06 -0400

    test

--- app/models/game.rb
+++ app/models/game.rb
@@ -6,6 +6,7 @@ class Game < ApplicationRecord
    validates :winner,              :presence => true
    validate  :validate_players
 
+
    def validate_players
      if home_user == away_user
        errors.add(:base, "Both the home and away user can't be the same! Please make sure that each player is a unique user.")
